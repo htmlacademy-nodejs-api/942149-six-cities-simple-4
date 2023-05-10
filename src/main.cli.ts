722765438ee2f077d92827e4c814dfcc7b58d112
Artem Kartaviy <a.kartavyj@idaproject.com>
2023-05-10 18:52:02 +0300

    feat(main.cli): добавляю генератор

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -4,9 +4,10 @@ import CliApp from './app/cli.js';
 import Help from './core/cli-command/help.command.js';
 import Version from './core/cli-command/version.command.js';
 import Import from './core/cli-command/import.command.js';
+import Generator from './core/cli-command/generate-command.js';
 
 const cliManager = new CliApp();
 
-cliManager.registerCommand([new Help(), new Version(), new Import()]);
+cliManager.registerCommand([new Help(), new Version(), new Import(), new Generator()]);
 
 cliManager.processCommand(process.argv);
