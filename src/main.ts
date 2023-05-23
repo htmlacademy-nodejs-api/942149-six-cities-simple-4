9f3ad7ea56a6396418d81b6c40b97ac6f9bcddde
Artem Kartaviy <a.kartavyj@idaproject.com>
2023-05-23 23:22:21 +0300

    feat(db): подключение к базе данных

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { Container } from 'inversify';
 import PinoService from './core/logger/pino.service.js';
 import RestApplication from './app/rest.js';
 import ConfigService from './core/config/config.service.js';
+import MongoClientService from './core/database-client/mongo-client.service.js';
 import { AppComponent } from './types/app-component.enum.js';
 import { LoggerInterface } from './core/logger/logger.interface.js';
 import { ConfigInterface } from './core/config/config.interface.js';
+import { DatabaseClientInterface } from './core/database-client/database-client.interface';
 import { RestSchema } from './core/config/rest.schema.js';
 
 async function bootstrap() {
@@ -14,6 +16,7 @@ async function bootstrap() {
   container.bind<RestApplication>(AppComponent.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<LoggerInterface>(AppComponent.LoggerInterface).to(PinoService).inSingletonScope();
   container.bind<ConfigInterface<RestSchema>>(AppComponent.ConfigInterface).to(ConfigService).inSingletonScope();
+  container.bind<DatabaseClientInterface>(AppComponent.DatabaseClientInterface).to(MongoClientService).inSingletonScope();
 
   const application = container.get<RestApplication>(AppComponent.RestApplication);
   await application.init();
