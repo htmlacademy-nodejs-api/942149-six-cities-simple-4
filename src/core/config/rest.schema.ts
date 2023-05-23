f91f812476023b2ef4ddf12da1bdea2993fcfa8c
Artem Kartaviy <a.kartavyj@idaproject.com>
2023-05-23 19:16:39 +0300

    feat(rest.scheme): расширяю схему

--- src/core/config/rest.schema.ts
+++ src/core/config/rest.schema.ts
@@ -5,6 +5,8 @@ convict.addFormats(validator);
 
 export type RestSchema = {
   APP_PORT: number;
+  SALT: string;
+  DB_HOST: string;
 }
 
 export const configRestSchema = convict<RestSchema>({
@@ -14,4 +16,16 @@ export const configRestSchema = convict<RestSchema>({
     env: 'APP_PORT',
     default: 5500
   },
+  SALT: {
+    doc: 'Salt for password hash',
+    format: String,
+    env: 'SALT',
+    default: null
+  },
+  DB_HOST: {
+    doc: 'IP address of the database server (MongoDB)',
+    format: 'ipaddress',
+    env: 'DB_HOST',
+    default: '127.0.0.1'
+  }
 });
