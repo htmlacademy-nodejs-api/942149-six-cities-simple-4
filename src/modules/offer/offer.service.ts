53790abdbb43c772f5999474f6bd22b2d1775a0a
Artem Kartaviy <a.kartavyj@idaproject.com>
2023-05-31 16:46:23 +0300

    feat(offers): дорабатываю список предложений

--- src/modules/offer/offer.service.ts
+++ src/modules/offer/offer.service.ts
@@ -1,10 +1,12 @@
 import { inject, injectable } from 'inversify';
-import CreateOfferDto from './dto/create-offer.dto.js';
 import { DocumentType, types } from '@typegoose/typegoose';
 import { OfferEntity } from './offer.entity.js';
 import { AppComponent } from '../../types/app-component.enum.js';
 import { LoggerInterface } from '../../core/logger/logger.interface.js';
 import { OfferServiceInterface } from './offer-service.interface.js';
+import UpdateOfferDto from './dto/update-offer.dto';
+import CreateOfferDto from './dto/create-offer.dto.js';
+import {DEFAULT_OFFER_COUNT} from './offer.constant';
 
 @injectable()
 export default class OfferService implements OfferServiceInterface {
@@ -22,5 +24,34 @@ export default class OfferService implements OfferServiceInterface {
 
   public async findById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
     return this.offerModel.findById(offerId).exec();
+  }
+
+  public async find(): Promise<DocumentType<OfferEntity>[]> {
+    return this.offerModel
+      .find()
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndDelete(offerId)
+      .exec();
+  }
+
+  public async updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, dto, {new: true})
+      .populate(['userId', 'categories'])
+      .exec();
+  }
+
+  public async findSomeOffers(count: number): Promise<DocumentType<OfferEntity>[]> {
+    const limit = count ?? DEFAULT_OFFER_COUNT;
+    return this.offerModel
+      .find()
+      .limit(limit)
+      .populate(['userId'])
+      .exec();
   }
 }
